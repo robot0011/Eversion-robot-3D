ccea4ca230f535ddec6db9688ece29355ed975f5
robot0011 <87816426+robot0011@users.noreply.github.com>
2025-05-29 10:50:39 +0700

    Remove installation instructions, update training script for extended training duration, and add requirements file.

--- training_eversion_robot_3d.py
+++ training_eversion_robot_3d.py
@@ -1,13 +1,21 @@
 import gymnasium as gym
-
 from stable_baselines3 import PPO
-
 from eversion_robot_3d import EversionRobot3D
 
-
+# Create environment
 env = EversionRobot3D(obs_use=True)
 
-model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=5000)
+# Set up PPO with TensorBoard logging
+model = PPO(
+    "MlpPolicy",
+    env,
+    verbose=1,
+    tensorboard_log="./ppo_eversion_tensorboard/"
+)
+
+# Train the model and log to TensorBoard under the "eversion_run" subdirectory
+model.learn(total_timesteps=3000000, tb_log_name="eversion_run")
+
+# Save the model
 model.save("ppo_eversion")
 print("Done")
